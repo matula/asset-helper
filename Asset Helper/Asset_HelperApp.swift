c4831cc4c769d9368adcf69781977ff0a7b6782b
Terry Matula <terrymatula@gmail.com>
2025-11-06 22:44:16 -0600

    Initial Commit

---
+++ Asset Helper/Asset_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asset_HelperApp.swift
+//  Asset Helper
+//
+//  Created by Terry Matula on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Asset_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
